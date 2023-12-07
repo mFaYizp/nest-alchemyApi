a99556dec918997e1e0223311d3afc8aa5fb5e69
mFaYizp <fayizfaz78@gmail.com>
2023-12-07 08:40:46 +0530

    authorized every endpoints

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
   @Get('transfers')
+  @UseGuards(AuthGuard())
   async getTransaction(@Res() res: Response) {
     try {
       const transfers = await this.walletService.getTransferTransaction();
+
       res.json({ message: 'Success', transfers });
     } catch (error) {
       console.log(error);
@@ -17,6 +20,7 @@ export class WalletController {
   }
 
   @Get('token/metadata/:address')
+  @UseGuards(AuthGuard())
   async getMetaInfo(@Param('address') token: string, @Res() res: Response) {
     try {
       const tokenInfo = await this.walletService.getMetadata(token);
@@ -27,6 +31,7 @@ export class WalletController {
   }
 
   @Get('token/balance/:address')
+  @UseGuards(AuthGuard())
   async getTokenBalance(@Param('address') token: string, @Res() res: Response) {
     try {
       const tokenBalance = await this.walletService.getTokenBalance(token);
@@ -37,6 +42,7 @@ export class WalletController {
   }
 
   @Get('nft/:address')
+  @UseGuards(AuthGuard())
   async getNftAddress(@Param('address') token: string, @Res() res: Response) {
     try {
       const nftAddress = await this.walletService.getNtfsAddress(token);
--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [WalletController],
   providers: [WalletService],
 })
